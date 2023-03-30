c0d3b53e23060f6bc9f6e653b678a3de6ab38d33
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2023-03-30 22:23:14 +0200

    Logger module added.

---
+++ logger.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+
+def get_logger(name):
+    # Logger instance
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # File handler
+    log_file = os.path(os.path.dirname(__file__), "bot.log")
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # Formatter
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s %(name)s %(message)s')
+
+    # Add handlers to the loggers and set the formatter
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    return logger
